93e0c4905c33a660c15385cb9177eb700b9944f2
Ethan Chen <jonathanchenzheng@hotmail.com>
2018-06-30 22:01:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,3 +3,11 @@ def current_player(board)
 end
 
 def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || spaces == "O"
+      counter += 1
+    end
+  end
+  counter
+end
